084572a7d3d5360d1d6a4b07790058977b551322
João Cardoso <36920247+Drarkin@users.noreply.github.com>
2018-02-28 15:29:21 +0000

    Update main.c

--- main.c
+++ main.c
@@ -11,13 +11,103 @@ Made by Drarkin (jefc)
 #include <string.h>
 #include <unistd.h>
 
+#define app_service
+#define debugInfo
+
 void myerr(int code,char* msg){
 	fprintf(stderr,"Err_%d",code);
 	if (msg!=NULL) fprintf(stderr,":%s",msg);
 	fprintf(stderr,"\n");
 	exit(-code);
 	}
+//configuration of App
+
+//Geral Default Values
+char	DV_csName[]="tejo.tecnico.ulisboa.pt";
+int	DV_cspt=59000;
+
+
+struct hostent *csh;
+struct in_addr *csa;
+
+//Config Params [CentralServer]
+char 		csip[16];	// ipv4 string max size
+unsigned int 	cspt=DV_cspt;	//port
+
+#ifdef app_reqserv
+	//nothing especific
+#elif app_service
+	//Default Values
+	int	DV_spt=59000;
+	//Config Params
+	unsigned int	id;
+	char		ip[16];
+	unsigned int	upt;
+	unsigned int	tpt;
+#else
+	#error app type not defined
+	#include <Error_App_NotDef>
+#endif
+
+void appConfg (char **arg){
+	//arg is a vector of string,
+	//arg last pointer must be NULL
+	int i=0;
+	char *SName=DV_csName;//CentralServer Name
+	#ifdef app_service
+	int argCheck = 0;
+	#endif
+	while (argv[i]!=NULL){
+		#ifdef app_service
+		if (strcmp(argv[i],"-n")==0 && argc>i){
+			i++;
+			argCheck++;
+			id=atoi(argv[i]);
+		}else	if (strcmp(argv[i],"-j")==0 && argc>i){
+			i++;
+			argCheck++;
+			ip=atoi(argv[i]);
+		}else   if (strcmp(argv[i],"-u)==0 && argc>i){
+                        i++;
+			argCheck++;
+			upt=atoi(argv[i]);
+                }else   if (strcmp(argv[i],"-t")==0 && argc>i){
+                        i++;
+			argCheck++;
+			tpt=atoi(argv[i]);
+                }
+		#endif
+		iif (strcmp(argv[i],"-i")==0 && argc>i){
+			i++;
+			SName=argv[i];
+		}else	if (strcmp(argv[i],"-p")==0 && argc>i){
+			i++;
+			cspt=atoi(argv[i]);
+		}
+		i++;
+	}
+	//check arguments for service
+	#ifdef app_service
+	if(argCheck!=4)myerr(1,"missing arguments");
+	#endif
+	//get CentralServer IP
+	csh = gethostbyname(SName);
+	if (h==NULL) myerr(2,"Fail to gethostbyname");
+	csa=(struct in_addr*)h->h_addr_list[0];
+				   
+	#ifdef debugInfo
+		#ifdef app_service
+		fprintf(stderr,"id: %d\nip: %d\nupt: %d\ntpt: %d\n",id,ip,upy,tpt);
+		#endif
+	fprintf(stderr,"CentralServer: %s:c%d\n",SName,cspt);
+	fprintf(stderr,"\tofficial host name: %s\n",h->h_name);
+	fprintf(stderr,"\tinternet address: %s (%08lX)\n",inet_ntoa(*a),(long unsigned int)ntohl(a->s_addr));
+ 	#endif
+	return;
+}
 int main (int argc, char **argv) {
+	appConfig(argv);
+/**LAb1 code	
 //jefc
         int SPort=58000;//default value
         char *SName="tejo";//default value
@@ -82,6 +172,6 @@ int main (int argc, char **argv) {
 	write(1,buffer,n);
 	write(1,"\n",1);
 	close(fd);
-
+//End Lab1Code**/
 	exit(0);
 }
