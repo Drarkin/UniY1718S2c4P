84f9839c2a0ff2b5c48e27667f7a1684c8bfc2af
João Cardoso <36920247+Drarkin@users.noreply.github.com>
2018-02-28 14:24:48 +0000

    Update main.c
    Update code from lab1

--- main.c
+++ main.c
@@ -10,6 +10,15 @@ Made by Drarkin (jefc)
 #include <string.h>
 #include <unistd.h>
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+
 void myerr(int code,char* msg){
 	fprintf(stderr,"Err_%d",code);
 	if (msg!=NULL) fprintf(stderr,":%s",msg);
@@ -22,12 +31,30 @@ int main (int argc, char **argv) {
         char *SName="tejo";//default value
 	char msg[1025];
 	int i=0;
+	int auxi=0;
+	int Stimes=1;
 	while (argv[i]!=NULL){
 		if (strcmp(argv[i],"-m")==0 && argc>i){
-			
-		}
+			i++;
+			auxi=strlen(argv[i]);
+			if (auxi>1024)strncpy(msg,argv[i],1024);
+			else strcpy(msg,argv[i]); 
+		}else	if (strcmp(argv[i],"-n")==0 && argc>i){
+			i++;
+			SName=argv[i];
+		}else   if (strcmp(argv[i],"-p")==0 && argc>i){
+                        i++;
+                        SPort=atoi(argv[i]);
+                }else   if (strcmp(argv[i],"-c")==0 && argc>i){
+                        i++;
+                        Stimes=atoi(argv[i]);
+                }
 
 		i++;
+	}
+	if ( auxi==0){
+		auxi=7;
+		strcmp(msg,"Hello!\n");
 	}
 //First Part
 	struct hostent *h = gethostbyname(SName);
@@ -47,8 +74,11 @@ int main (int argc, char **argv) {
 	addr.sin_family=AF_INET;
 	addr.sin_addr=*a;//MachineIP
 	addr.sin_port=htons(SPort);
-	n=sendto(fd,"Hello!\n",7,0,(struct sockaddr*)&addr,sizeof(addr));
-	if(n==-1)myerr(2,"Fail to send");
+	while(0!=Stimes--){
+		n=sendto(fd,msg,auxi,0,(struct sockaddr*)&addr,sizeof(addr));
+		if(n==-1)myerr(2,"Fail to send");
+		fprintf(stdout,">Sent! (%d)\n",Stimes);
+	}
 //Third Part
 	int addrlen;
 	char buffer[128];
@@ -58,6 +88,7 @@ int main (int argc, char **argv) {
 	if(n==-1)myerr(3,"Fail to recive data");//error
 	write(1,"echo: ",6);//stdout
 	write(1,buffer,n);
+	write(1,"\n",1);
 	close(fd);
 
 	exit(0);
